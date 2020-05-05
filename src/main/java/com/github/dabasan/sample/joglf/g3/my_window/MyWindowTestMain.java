7edf9fd1f07b9145d30f7dd6f07df2fc7ab42477
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-05 14:47:13 +0900

    Updated MyWindowTestMain.

--- src/main/java/com/github/dabasan/sample/joglf/g3/my_window/MyWindowTestMain.java
+++ src/main/java/com/github/dabasan/sample/joglf/g3/my_window/MyWindowTestMain.java
@@ -1,18 +1,13 @@
 package com.github.dabasan.sample.joglf.g3.my_window;
 
-import com.github.dabasan.joglf.gl.front.GLFront;
 import com.github.dabasan.joglf.gl.window.JOGLFWindowInterface;
-import com.github.dabasan.joglf.gl.wrapper.GLVersion;
 
 public class MyWindowTestMain {
 	public static void main(String[] args) {
 		new MyWindowTestMain();
 	}
 	public MyWindowTestMain() {
-		GLFront.Setup(GLVersion.GL3);
-
 		JOGLFWindowInterface window = new MyWindow();
 		window.SetTitle("MyWindow");
-		window.SetExitProcessWhenDestroyed();
 	}
 }
