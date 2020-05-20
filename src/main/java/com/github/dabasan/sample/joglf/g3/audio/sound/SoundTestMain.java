44da21c93a541175a7e1498a9ee231a45ba5c4a7
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-20 19:37:03 +0900

    Update

--- src/main/java/com/github/dabasan/sample/joglf/g3/audio/sound/SoundTestMain.java
+++ src/main/java/com/github/dabasan/sample/joglf/g3/audio/sound/SoundTestMain.java
@@ -1,15 +1,10 @@
 package com.github.dabasan.sample.joglf.g3.audio.sound;
 
-import com.github.dabasan.joglf.gl.front.GLFront;
-import com.github.dabasan.joglf.gl.wrapper.GLVersion;
-
 public class SoundTestMain {
 	public static void main(String[] args) {
 		new SoundTestMain();
 	}
 	public SoundTestMain() {
-		GLFront.Setup(GLVersion.GL3);
-
 		var window = new Sound3DTestWindow();
 		window.SetExitProcessWhenDestroyed();
 	}
