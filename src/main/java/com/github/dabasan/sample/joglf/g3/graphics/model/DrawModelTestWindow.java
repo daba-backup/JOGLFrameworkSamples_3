05792d047781fa368675016ce0f92e1fea5deadb
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-22 22:34:41 +0900

    Added a sample to draw a model.

---
+++ src/main/java/com/github/dabasan/sample/joglf/g3/graphics/model/DrawModelTestWindow.java
@@ -0,0 +1,27 @@
+package com.github.dabasan.sample.joglf.g3.graphics.model;
+
+import static com.github.dabasan.basis.vector.VectorFunctions.*;
+
+import com.github.dabasan.joglf.gl.front.CameraFront;
+import com.github.dabasan.joglf.gl.model.Model3DFunctions;
+import com.github.dabasan.joglf.gl.window.JOGLFWindow;
+
+class DrawModelTestWindow extends JOGLFWindow {
+	private int model_handle;
+
+	@Override
+	public void Init() {
+		model_handle = Model3DFunctions.LoadModel("./Data/Model/BD1/Cube/cube.bd1");
+	}
+
+	@Override
+	public void Update() {
+		CameraFront.SetCameraPositionAndTarget_UpVecY(VGet(30.0f, 30.0f, 30.0f),
+				VGet(0.0f, 0.0f, 0.0f));
+	}
+
+	@Override
+	public void Draw() {
+		Model3DFunctions.DrawModel(model_handle);
+	}
+}
---
+++ src/main/java/com/github/dabasan/sample/joglf/g3/graphics/model/ModelMain.java
@@ -0,0 +1,11 @@
+package com.github.dabasan.sample.joglf.g3.graphics.model;
+
+public class ModelMain {
+	public static void main(String[] args) {
+		new ModelMain();
+	}
+	public ModelMain() {
+		var window = new DrawModelTestWindow();
+		window.SetTitle("Draw Model");
+	}
+}
