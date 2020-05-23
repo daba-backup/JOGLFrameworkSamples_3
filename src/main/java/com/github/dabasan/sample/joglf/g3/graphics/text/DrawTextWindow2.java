df93867754e3bd4839deedc5c26812ecde269540
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-23 10:39:00 +0900

    Updated a sample.

--- src/main/java/com/github/dabasan/sample/joglf/g3/graphics/text/DrawTextWindow2.java
+++ src/main/java/com/github/dabasan/sample/joglf/g3/graphics/text/DrawTextWindow2.java
@@ -8,23 +8,35 @@ import com.github.dabasan.joglf.gl.text.TextMgr;
 import com.github.dabasan.joglf.gl.window.JOGLFWindow;
 
 class DrawTextWindow2 extends JOGLFWindow {
-	int font_handle;
+	int meiryo_handle;
+	int batang_handle;
+	int fangsong_handle;
+
 	int dpi;
 
 	@Override
 	public void Init() {
-		font_handle = TextMgr.LoadFont("C:/Windows/Fonts/meiryo.ttc");
+		meiryo_handle = TextMgr.LoadFont("C:/Windows/Fonts/meiryo.ttc");
+		batang_handle = TextMgr.LoadFont("C:/Windows/Fonts/batang.ttc");
+		fangsong_handle = TextMgr.LoadFont("C:/Windows/Fonts/simfang.ttf");
+
 		dpi = Toolkit.getDefaultToolkit().getScreenResolution();
 	}
 
 	@Override
 	public void Draw() {
-		String text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		String text1 = "В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!";
 		String text2 = "人はいさ心も知らずふるさとは花ぞ昔の香ににほひける";
+		String text3 = "한글 폰트 테스트";
+		String text4 = "永和九年，岁在癸丑，暮春之初，会于会稽山阴之兰亭，修禊事也。";
 
-		TextMgr.DrawTextWithFont(5, 20, text1, font_handle, GetColorU8(1.0f, 1.0f, 1.0f, 1.0f), 24,
-				dpi);
-		TextMgr.DrawTextWithFont(5, 70, text2, font_handle, GetColorU8(1.0f, 1.0f, 1.0f, 1.0f), 24,
-				dpi);
+		TextMgr.DrawTextWithFont(5, 20, text1, meiryo_handle, GetColorU8(1.0f, 1.0f, 1.0f, 1.0f),
+				24, dpi);
+		TextMgr.DrawTextWithFont(5, 70, text2, meiryo_handle, GetColorU8(1.0f, 1.0f, 1.0f, 1.0f),
+				24, dpi);
+		TextMgr.DrawTextWithFont(5, 120, text3, batang_handle, GetColorU8(1.0f, 1.0f, 1.0f, 1.0f),
+				24, dpi);
+		TextMgr.DrawTextWithFont(5, 170, text4, fangsong_handle, GetColorU8(1.0f, 1.0f, 1.0f, 1.0f),
+				24, dpi);
 	}
 }
